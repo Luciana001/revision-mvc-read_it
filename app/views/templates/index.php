0921858f649d260c237ce8a41a2afa589d2c5c08
Luciana <luciana.brancato@gmail.com>
2022-06-20 15:36:08 +0200

    Intégration du template statique + découpe en partials

---
+++ app/views/templates/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include '../app/views/templates/partials/_head.php' ?>
+</head>
+
+<body>
+
+    <?php include '../app/views/templates/partials/_nav.php' ?>
+    <!-- END nav -->
+
+    <!-- main -->
+    <?php include_once '../app/views/templates/partials/_main.php' ?>
+
+    <!-- footer -->
+    <?php include_once '../app/views/templates/partials/_footer.php' ?>
+
+
+    <!-- loader -->
+    <?php include_once '../app/views/templates/partials/_loader.php' ?>
+
+
+    <!-- scripts -->
+    <?php include_once '../app/views/templates/partials/_scripts.php' ?>
+
+</body>
+
+</html>
