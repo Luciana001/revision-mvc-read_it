ea1c88cf7439e005e1a029b9bab9cfe4ce92416a
Luciana <luciana.brancato@gmail.com>
2022-06-20 15:11:30 +0200

    création de la structure MVC et configuration des fichiers de connexion à la base de donnée.

---
+++ app/config/params.php
@@ -0,0 +1,9 @@
+<?php
+//initialisation des zones dynamiques
+$content = "";
+
+//parametres de connexion
+define('DB_HOST','localhost:3306');
+define('DB_NAME','readit');
+define('DB_USER','root');
+define('DB_PASSWORD','root');
